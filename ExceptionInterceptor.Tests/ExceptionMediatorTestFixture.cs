53b08bb01d3b3f2829b65c45ac8f02bd09763b9f
Khwezi Mngoma <khwezi@mngoma.co.za>
2019-04-28 18:10:04 +0200

    Add project files.

---
+++ ExceptionInterceptor.Tests/ExceptionMediatorTestFixture.cs
@@ -0,0 +1,86 @@
+﻿using ExceptionMediatorLibrary;
+using NUnit.Framework;
+using System;
+
+namespace ExceptionMediator.Tests
+{
+    [TestFixture]
+    public class ExceptionMediatorTestFixture
+    {
+        [Test]
+        public void ExceptionMediator_OnException_ResponseTupleExceptionMustNotBeNull()
+        {
+            var result = ExceptionMediator.Intercept<object>(() => throw new Exception());
+
+            Assert.That(result.exception, Is.Not.Null);
+            Assert.That(result.routineResult, Is.Null);
+
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void ExceptionMediator_OnFormatException_ResponseTupleFormatExceptionMustNotBeNull()
+        {
+            var invalidFormatInput = "A";
+
+            var result = ExceptionMediator.Intercept<object>(() =>
+            {
+                return int.Parse(invalidFormatInput);
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.exception, Is.Not.Null);
+                Assert.That(result.exception.GetType(), Is.EqualTo(typeof(FormatException)));
+                Assert.That(result.routineResult, Is.Null);
+            });
+
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void ExceptionMediator_OnExplicitReturnTypeCallWithNoException_PassOnValidResultModel()
+        {
+            var invalidFormatInput = "1";
+
+            var result = ExceptionMediator.Intercept<int>(() =>
+            {
+                return int.Parse(invalidFormatInput);
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.GetType(), Is.EqualTo(typeof((Exception, int))));
+                Assert.That(result.exception, Is.Null);
+
+                Assert.That(result.routineResult, Is.Not.Null);
+                Assert.That(result.routineResult.GetType(), Is.EqualTo(typeof(int)));
+                Assert.That(result.routineResult, Is.Not.EqualTo(default));
+                Assert.That(result.routineResult, Is.EqualTo(1));
+            });
+
+            Console.WriteLine(result);
+        }
+
+        [Test]
+        public void ExceptionMediator_OnImpliedReturnTypeCallWithNoException_PassOnValidResultModel()
+        {
+            var invalidFormatInput = "1";
+
+            var result = ExceptionMediator.Intercept(() =>
+            {
+                return int.Parse(invalidFormatInput);
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.exception, Is.Null);
+                Assert.That(result.routineResult.GetType(), Is.EqualTo(typeof(int)));
+                Assert.That(result.routineResult, Is.EqualTo(1));
+                Assert.That(result.routineResult, Is.Not.EqualTo(default));
+            });
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ExceptionMediatorExtensions/ExceptionMediator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExceptionMediatorLibrary
+{
+    public static class ExceptionMediator
+    {
+        public static (Exception exception, TReturn routineResult) Intercept<TReturn>(Func<TReturn> routine) 
+        {
+            var routineResult = default(TReturn);
+
+            try
+            {
+                routineResult = routine();
+
+                return (null, routineResult);
+            }
+            catch(Exception ex)
+            {
+                return (ex, default);
+            }            
+        }
+    }
+}
